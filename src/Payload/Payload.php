468b6fd1f6a52a5424564444803cb1564e2aef3c
Milad Nekofar <milad@nekofar.com>
2021-09-16 23:51:30 +0430

    chore: introduce jsend payload as a json serializable class

---
+++ src/Payload/Payload.php
@@ -0,0 +1,103 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nekofar\Slim\JSend\Payload;
+
+use JsonSerializable;
+
+/**
+ *
+ */
+class Payload implements JsonSerializable
+{
+    /**
+     * @var PayloadStatus
+     */
+    private $status;
+
+    /**
+     * @var mixed|null
+     */
+    private $data;
+
+    /**
+     * @var string|null
+     */
+    private $message;
+
+    /**
+     * @var integer|null
+     */
+    private $code;
+
+    /**
+     * @param PayloadStatus $status
+     * @param mixed|null    $data
+     * @param string|null   $message
+     * @param integer|null  $code
+     */
+    public function __construct(PayloadStatus $status, $data = null, ?string $message = null, ?int $code = null)
+    {
+        $this->status = $status;
+        $this->data = $data;
+        $this->message = $message;
+        $this->code = $code;
+    }
+
+    /**
+     * @param mixed|null $data
+     *
+     * @return self
+     */
+    public static function success($data = null): self
+    {
+        return new self(PayloadStatus::fromString(PayloadStatus::STATUS_SUCCESS), $data);
+    }
+
+    /**
+     * @param mixed|null $data
+     *
+     * @return self
+     */
+    public static function fail($data = null): self
+    {
+        return new self(PayloadStatus::fromString(PayloadStatus::STATUS_FAIL), $data);
+    }
+
+    /**
+     * @param string       $message
+     * @param integer|null $code
+     * @param mixed|null   $data
+     *
+     * @return self
+     */
+    public static function error(string $message, ?int $code = null, $data = null): self
+    {
+        return new self(PayloadStatus::fromString(PayloadStatus::STATUS_ERROR), $data, $message, $code);
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function jsonSerialize(): array
+    {
+        $payload = [
+            'status' => $this->status,
+        ];
+
+        if ($this->data !== null) {
+            $payload['data'] = $this->data;
+        }
+
+        if ($this->message !== null) {
+            $payload['message'] = $this->code;
+        }
+
+        if ($this->code !== null) {
+            $payload['code'] = $this->code;
+        }
+
+        return $payload;
+    }
+}
---
+++ src/Payload/PayloadStatus.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nekofar\Slim\JSend\Payload;
+
+/**
+ *
+ */
+class PayloadStatus
+{
+    public const STATUS_SUCCESS = 'success';
+    public const STATUS_FAIL = 'fail';
+    public const STATUS_ERROR = 'error';
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @param string $name
+     */
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @param string $status
+     *
+     * @return self
+     */
+    public static function fromString(string $status): self
+    {
+        return new self($status);
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->name;
+    }
+}
