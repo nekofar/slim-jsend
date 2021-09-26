d2d2743f8b171e42caad9079fb9926ab0bf9a33b
Milad Nekofar <milad@nekofar.com>
2021-09-26 13:05:15 +0330

    test: add new test class for testing `PayloadStatus`

---
+++ tests/PayloadStatusTest.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * (c) Milad Nekofar <milad@nekofar.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use InvalidArgumentException;
+use Nekofar\Slim\JSend\PayloadStatus;
+use PHPUnit\Framework\TestCase;
+
+/**
+ *
+ */
+class PayloadStatusTest extends TestCase
+{
+    /**
+     *
+     */
+    public function testFromString(): void
+    {
+        $actual = PayloadStatus::fromString('success');
+        $expected = new PayloadStatus('success');
+
+        self::assertEquals($expected, $actual);
+        self::assertSame((string) $expected, (string) $actual);
+    }
+
+    /**
+     *
+     */
+    public function testWithInvalidString(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid status name given');
+
+        PayloadStatus::fromString('invalid');
+    }
+}
